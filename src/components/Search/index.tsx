63f92176a74812c3bf065d6fc7d18ab0ab93873e
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-13 11:52:15 -0300

    Feat: Add autocomplete to the Search component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,10 +5,44 @@ import styles from "./styles.module.scss";
 export default function Search() {
   return (
     <div className={styles.container}>
-      <input placeholder="Buscar produtos..." type="text" />
-      <button>
-        <FiSearch size={20} color="#BBB" />
-      </button>
+      <div className={styles.input}>
+        <input placeholder="Buscar produtos..." type="text" />
+        <button>
+          <FiSearch size={20} color="#BBB" />
+        </button>
+      </div>
+
+      <div className={styles.autoComplete}>
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>air fryer</p>
+        </div>
+
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>celular motorola moto g 30</p>
+        </div>
+
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>ar condicionado</p>
+        </div>
+
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>bicicleta aro 29</p>
+        </div>
+
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>a32</p>
+        </div>
+
+        <div>
+          <FiSearch size={20} color="#560BAD" />
+          <p>fritadeira airfryer</p>
+        </div>
+      </div>
     </div>
   );
 }
