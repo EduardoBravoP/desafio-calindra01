2ff394d645ebc22cb95f497dedf03eb7a48c0f93
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-13 20:31:19 -0300

    Feat: Add product listing

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -2,23 +2,26 @@ import { FiBarChart, FiEye } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
-export default function Product() {
+interface ProductProps {
+  name: string;
+  score: number;
+  clicksCount: string;
+}
+
+export default function Product({ name, score, clicksCount }: ProductProps) {
   return (
     <div className={styles.container}>
-      <h3>
-        Smartphone Motorola Moto G30 128GB 4G Wi-Fi Tela 6.5 Dual Chip 4GB RAM
-        Câmera Quádrupla + Selfie 13MP - Dark Prism
-      </h3>
+      <h3>{name}</h3>
 
       <div className={styles.footer}>
         <div>
           <FiBarChart size={20} color="#560BAD" />
-          <span>100.000</span>
+          <span>{score}</span>
         </div>
 
         <div>
           <FiEye size={20} color="#560BAD" />
-          <span className={styles.eye}>7482</span>
+          <span className={styles.eye}>{clicksCount}</span>
         </div>
       </div>
     </div>
