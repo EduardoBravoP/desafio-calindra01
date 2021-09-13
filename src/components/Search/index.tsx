1f43ded7ccb76f2b6da25006239c5f8ba22f51d8
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-13 20:31:54 -0300

    Feat: Add suggestions from api

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,48 +1,85 @@
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
 
-export default function Search() {
+interface Product {
+  _meta: {
+    score: number;
+    visitsClickCount: string;
+  };
+  name: string;
+  id: string;
+}
+
+interface Suggestion {
+  term: string;
+}
+
+interface Data {
+  products: Product[];
+  suggestions: Suggestion[];
+}
+
+interface SearchProps {
+  setProducts: Dispatch<SetStateAction<Product[] | undefined>>;
+}
+
+export default function Search({ setProducts }: SearchProps) {
+  const [data, setData] = useState<Data>();
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
+  const [isSuggestionShown, setIsSuggestionShown] = useState(false);
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+    setProducts(data?.products);
+  }
+
+  async function handleSearchValueChange(value: string) {
+    if (!value.trim()) {
+      setIsSuggestionShown(false);
+      setSuggestions([]);
+
+      return;
+    }
+
+    setIsSuggestionShown(true);
+
+    const response = await api.get<Data>(
+      `/autocomplete?content=${value}&source=nanook`
+    );
+
+    setSuggestions(response.data.suggestions);
+    setData(response.data);
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.input}>
-        <input placeholder="Buscar produtos..." type="text" />
-        <button>
+      <form className={styles.input} onSubmit={handleSearch}>
+        <input
+          placeholder="Buscar produtos..."
+          type="text"
+          onChange={(e) => handleSearchValueChange(e.target.value)}
+          onBlur={() => setIsSuggestionShown(false)}
+          onFocus={() => setIsSuggestionShown(true)}
+        />
+        <button type="submit">
           <FiSearch size={20} color="#BBB" />
         </button>
-      </div>
+      </form>
 
-      <div className={styles.autoComplete}>
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>air fryer</p>
+      {isSuggestionShown && suggestions[0] && (
+        <div className={styles.autoComplete}>
+          {suggestions.map((suggestion) => (
+            <div key={suggestion.term}>
+              <FiSearch size={20} color="#560BAD" />
+              <p>{suggestion.term}</p>
+            </div>
+          ))}
         </div>
-
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>celular motorola moto g 30</p>
-        </div>
-
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>ar condicionado</p>
-        </div>
-
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>bicicleta aro 29</p>
-        </div>
-
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>a32</p>
-        </div>
-
-        <div>
-          <FiSearch size={20} color="#560BAD" />
-          <p>fritadeira airfryer</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,43 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Product from "../components/Product";
 import Search from "../components/Search";
 
 import styles from "../styles/pages/index.module.scss";
 
+interface Product {
+  _meta: {
+    score: number;
+    visitsClickCount: string;
+  };
+  name: string;
+  id: string;
+}
+
 const Home: NextPage = () => {
+  const [products, setProducts] = useState<Product[] | undefined>([]);
+
   return (
     <div className={styles.container}>
       <header>
-        <Search />
+        <Search setProducts={setProducts} />
       </header>
 
       <main>
         <h1>Produtos</h1>
         <div className={styles.list}>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {products ? (
+            products.map((product) => (
+              <Product
+                key={product.id}
+                name={product.name}
+                score={product._meta.score}
+                clicksCount={product._meta.visitsClickCount}
+              />
+            ))
+          ) : (
+            <p>Busque por produtos</p>
+          )}
         </div>
       </main>
     </div>
